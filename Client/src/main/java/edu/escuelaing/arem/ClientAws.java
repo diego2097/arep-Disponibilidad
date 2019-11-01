e259e4690196d9cea6b69bb0cb2794290735ab2d
diego2097 <ye.yo14@hotmail.com>
2019-11-01 14:02:37 -0500

    inicio

---
+++ Client/src/main/java/edu/escuelaing/arem/ClientAws.java
@@ -0,0 +1,30 @@
+package edu.escuelaing.arem;
+
+/**
+ * Hello world!
+ *
+ */
+public class ClientAws 
+{
+     public static int clients = 1000;
+    public static String url  = "http://localhost:4567/ordenar?cadena=";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+
+        ClientThread[] threads = new ClientThread[clients];
+
+        for (int i = 0; i < clients; i++) {
+            threads[i] = new ClientThread(url);
+        }
+        for (int i = 0; i < clients; i++) {
+            threads[i].start();
+        }
+        for (int i = 0; i < clients; i++) {
+            threads[i].join();
+        }
+        System.out.println("Request finish");
+    }
+}
